497f803bffc1219446703f76ae574c9423b24b37
Tomer <gmtomer@gmail.com>
2017-01-03 10:58:56 +0200

    basic package mock

--- __test__/models/repository.spec.js
+++ __test__/models/repository.spec.js
@@ -1,7 +1,15 @@
+jest.mock('../../lib/models/package');
 import Repository from '../../lib/models/repository';
 
 describe('repository', () => {
   it('should return packages', () => {
     expect(Repository.get).toBeInstanceOf(Array);
+  });
+  const react = {};
+  it(`should set a new package`, () => {
+    const oldPackagesCount = Repository.get.length;
+    Repository.set(react);
+    expect(Repository.get.length).toBe(oldPackagesCount + 1);
+    expect(Repository.packages.react).toBeDefined();
   });
 });
