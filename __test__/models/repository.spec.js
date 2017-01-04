158206614fd89219922c7230cec025bff6a605ee
Tomer <gmtomer@gmail.com>
2017-01-04 10:41:25 +0200

    moar tests

--- __test__/models/repository.spec.js
+++ __test__/models/repository.spec.js
@@ -6,11 +6,20 @@ describe('repository', () => {
   it('should return packages', () => {
     expect(Repository.get).toBeInstanceOf(Array);
   });
-  const react = new Package(require('./single_package'));
   it(`should set a new package`, () => {
+    const react = new Package(require('./single_package'));
     const oldPackagesCount = Repository.get.length;
     Repository.set(react);
     expect(Repository.get.length).toBe(oldPackagesCount + 1);
     expect(Repository.packages.react).toBeDefined();
+  });
+  it(`should update the previous package`, () => {
+    const react = new Package(require('./single_package'));
+    const react2 = new Package(require('./single_package'));
+    Repository.set(react);
+    expect(Repository.packages.react).toBe(react);
+    Repository.set(react2);
+    expect(Repository.packages.react).toBe(react2);
+    expect(Repository.packages.react === react).toBeFalsy();
   });
 });
